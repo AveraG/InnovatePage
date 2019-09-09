d950e1736a53e203da79bb649a99d52d6ab06ff0
Avera <54907937+AveraG@users.noreply.github.com>
2019-09-09 17:44:03 -0500

    Add files via upload

---
+++ kitchen sink/page.js
@@ -0,0 +1,43 @@
+let name = "Avera"; /* set variable with my name*/
+
+let sum = 5 + 4; /*set variable for sum of 5 and 4 */
+
+function sayHello () {
+    console.log("Hello!");
+}
+
+function checkAge(name, age) {
+     if (age < 21); {
+         console.log ("Sorry " + name + ", you aren't old enough to view this page!");
+     } 
+ }
+ checkAge ("Charles", 21);
+ checkAge ("Abby", 27);
+ checkAge ("Jeames", 18);
+ checkAge ("John", 17);
+
+
+ let favoriteVeggies = ["spinach", "brussel sprouts", "asparagus"]; 
+ for (i = 0; i < 2; i++) { 
+    console.log("favoriteVeggies");
+ }
+  
+let nameAge = [
+    {name: "sarah", age: 21},
+    {name: "James", age: 19},
+    {name: "Jacob", age: 27},
+    {name: "Jane", age: 20},
+    {name: "Cat", age: 30}
+]
+
+var i;
+for (i = 0; i < nameAge.length; i++) {
+    checkAge(nameAge[i]);
+}
+
+
+ function getLength() {
+    return getLength.length;
+ }
+
+ //let length = returnValue(getLength);
