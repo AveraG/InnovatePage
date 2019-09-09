622d0b7777156e384d128b30bdbed3c4bc31840f
Avera <54907937+AveraG@users.noreply.github.com>
2019-09-08 20:58:49 -0500

    Add files via upload

---
+++ AveraKitchenSink/app.js
@@ -0,0 +1,29 @@
+let name = "Avera"; /* set variable with my name*/
+
+let sum = 5 + 4; /*set variable for sum of 5 and 4 */
+
+function sayHello (); {
+    console.log("Hello!");
+}
+
+function checkAge(name, age); {
+     if (age < 21); {
+         console.log ("Sorry " + name + ", you aren't old enough to view this page!");
+     } 
+ }
+ checkAge ("Charles", 21)
+ checkAge("Abby", 27)
+ checkAge ("Jeames", 18)
+ checkAge ("John", 17)
+
+
+ let favoriteVeggies = ["spinach", "brussel sprouts", "asparagus"] 
+ for (i = 0; i < 2; i++) { 
+    console.log("favoriteVeggies")
+ }
+  
+ function getLength() {
+    return getLength.length
+ }
+
+ let length = returnValue(getLength)
